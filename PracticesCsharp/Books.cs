abdf6122781e7cf7e1dac5f38a3cc32bc574237f
Ketty Chung <k.chung@vanlanschotkempen.com>
2018-12-19 16:34:07 +0100

    Eerste opzetje

---
+++ PracticesCsharp/Books.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticesCsharp
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public bool ChildBook { get;set }
+
+    }
+}
---
+++ PracticesCsharp/Repository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticesCsharp
+{
+    class Repository : IRepository
+    {
+        //hoe nu verder
+    }
+}
